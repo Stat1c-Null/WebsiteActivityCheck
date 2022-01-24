440574be92d31351ee3829714743f3fc8ee833fb
Mikita Slabysh <slabysh2015@gmail.com>
2022-01-24 00:02:32 -0500

    Minor fixes

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import csv, subprocess, time
-from operator import delitem
 
 #Variables
 website = []
 status_code = []
 
 #Clear out old logs before testing
-with open('log.txt', 'w'):
+with open('WebsiteTester/log.txt', 'w'):
     pass
 
 #Read data from csv
-with open('data.csv', 'r') as csv_file:
+with open('WebsiteTester/data.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     #Save all the websites to array
@@ -21,11 +20,11 @@ with open('data.csv', 'r') as csv_file:
     for z in website:
         test_input = 'curl -s -o /dev/null -w "%{http_code}" ' + str(z)
         #Run CMD prompt and write the result into a file
-        subprocess.Popen(test_input + ' >> log.txt'.format(z),shell=True)
+        subprocess.Popen(test_input + ' >> WebsiteTester/log.txt'.format(z),shell=True)
         time.sleep(1.5)#Wait 1.5 seconds before testing next one, so processes won't overlap
     
     #Read output from file and save it in the array
-    file = open('log.txt', 'r')
+    file = open('WebsiteTester/log.txt', 'r')
     while True:
         char = file.read(3)#Read only first 3 characters
         if not char:
@@ -34,12 +33,13 @@ with open('data.csv', 'r') as csv_file:
     file.close()
 
 #Write results into csv file
-with open('data.csv', 'r') as file:
+with open('WebsiteTester/data.csv', 'r') as file:
     csv_reader = csv.reader(file)
-    with open('new_data.csv', 'w') as data_file:
+    with open('WebsiteTester/new_data.csv', 'w') as data_file:
         csv_writer = csv.writer(data_file, delimiter=",")
         counter = 0
         for line in csv_reader:
             line.append(status_code[counter])
             counter += 1
             csv_writer.writerow(line)
+print("Done")
